d56c033bb76c5cac99d52bfbb4c6bf063431f694
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-16 20:08:35 +0600

    Создал types который хранит глобальные интерфейсы

---
+++ src/types.d.ts
@@ -0,0 +1,13 @@
+export interface APICountry {
+  name: string;
+  alpha3Code: string
+}
+
+export interface APIOneCountry {
+  name: string;
+  capital: string;
+  flag: string;
+  population: number;
+  region: string;
+  subregion: string;
+}
